0bc31401dbf3d171386d06ddd58bd5bde733ebed
rabra <rabira.hierpa@gmail.com>
2024-11-30 22:34:56 +0300

    Add User module with TypeORM integration and user controller

--- backend/src/features/account/user/user.module.ts
+++ backend/src/features/account/user/user.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
