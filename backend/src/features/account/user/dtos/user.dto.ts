28b24e8f5e61986f441516cc75a2f1c39915d1a8
rabra <rabira.hierpa@gmail.com>
2024-12-01 09:51:55 +0300

    feat: Add UserDecodeDto for user data validation and structure

---
+++ backend/src/features/account/user/dtos/user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class UserDecodeDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsString()
+  fullName: string;
+}
