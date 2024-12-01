850b4f1fcd64ee2c298fe06c747d5b384533814e
rabra <rabira.hierpa@gmail.com>
2024-12-01 09:51:28 +0300

    feat: Extend User entity with password comparison and fullName getter

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -1,19 +1,36 @@
-import { Column, Entity, ObjectId, ObjectIdColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import * as bcrypt from 'bcrypt';
+import { Expose } from 'class-transformer';
+import { AbstractEntity } from 'src/shared/dtos/abstract.dto';
+import { Column, Entity } from 'typeorm';
 
 @Entity()
-export class User {
-  @ObjectIdColumn()
-  id: ObjectId;
-
+export class UserEntity extends AbstractEntity {
+  @ApiProperty()
   @Column()
   firstName: string;
 
   @Column()
+  @ApiProperty()
   lastName: string;
 
+  @ApiProperty()
   @Column({ unique: true })
   email: string;
 
+  @ApiProperty()
   @Column()
   password: string;
+
+  async comparePassword(attempt: string) {
+    return await bcrypt.compare(attempt, this.password);
+  }
+
+  @Expose()
+  get fullName(): string {
+    return (
+      [this.firstName, this.lastName].filter((name) => name).join(' ') ||
+      this.firstName
+    );
+  }
 }
