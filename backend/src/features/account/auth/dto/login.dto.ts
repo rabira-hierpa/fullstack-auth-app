b4f2f8c1013e7d92e82a0416353973790353b1c2
rabra <rabira.hierpa@gmail.com>
2024-12-01 09:51:43 +0300

    refactor: Enhance LoginDto with password comparison method and validation

--- backend/src/features/account/auth/dto/login.dto.ts
+++ backend/src/features/account/auth/dto/login.dto.ts
@@ -1,12 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import * as bcrypt from 'bcrypt';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({ example: 'user@example.com', description: 'User email' })
   @IsEmail()
+  @IsString()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty({ example: 'password123', description: 'User password' })
+  @IsString()
   @IsNotEmpty()
   password: string;
+
+  async comparePassword(attempt: string) {
+    return await bcrypt.compare(attempt, this.password);
+  }
 }
