d3e631798b4a212956726cf922c478beb1a5706f
rabra <rabira.hierpa@gmail.com>
2024-12-01 09:53:25 +0300

    refactor: Update user module to use UserEntity instead of User

--- backend/src/features/account/user/user.module.ts
+++ backend/src/features/account/user/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
+import { UserEntity } from 'src/entities/user.entity';
 import { UserController } from './controllers/user/user.controller';
 import { UserService } from './services/user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
