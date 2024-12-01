a137a349c78e35464caf5407c6d96699ffc51ee4
rabra <rabira.hierpa@gmail.com>
2024-12-02 00:05:48 +0300

    feat: Enhance UserEntity with password hashing and token exclusion

--- backend/src/entities/user.entity.ts
+++ backend/src/entities/user.entity.ts
@@ -1,8 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import * as bcrypt from 'bcrypt';
-import { Expose } from 'class-transformer';
+import { Exclude, Expose } from 'class-transformer';
 import { AbstractEntity } from 'src/shared/dtos/abstract.dto';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
 
 @Entity()
 export class UserEntity extends AbstractEntity {
@@ -21,6 +21,16 @@ export class UserEntity extends AbstractEntity {
   @ApiProperty()
   @Column()
   password: string;
+
+  @ApiProperty()
+  @Column({ nullable: true })
+  @Exclude()
+  token: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
 
   async comparePassword(attempt: string) {
     return await bcrypt.compare(attempt, this.password);
