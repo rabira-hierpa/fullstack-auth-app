4c23a73e363419b5c0bab4bca5bd0f854c568b95
rabra <rabira.hierpa@gmail.com>
2024-12-02 16:35:50 +0300

    feat: Refactor main entry point to include routing and authentication context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "antd/dist/reset.css"; // For Ant Design v5+
+import { AuthProvider } from "./shared/context/auth.context.tsx";
+import { environment } from "./features/account/environments/environment.dev.ts";
+import { LoadingSpinner } from "./shared/ui/spinner/loading.spinner.tsx";
+import AppRoutes from "./app.routes.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider environment={environment}>
+        <Suspense fallback={<LoadingSpinner fullScreen={true} />}>
+          <AppRoutes />
+        </Suspense>
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
