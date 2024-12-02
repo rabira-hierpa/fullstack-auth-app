4f11d7ea7b8c1721e563daaea4a5d4ae552c9c3d
rabra <rabira.hierpa@gmail.com>
2024-12-02 16:28:32 +0300

    feat: Integrate Tailwind CSS for improved styling and remove legacy CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html", // For Vite
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust for your project structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
