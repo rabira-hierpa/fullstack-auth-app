f8f08c10b294743f424660c9f0ad6ba38e664032
rabra <rabira.hierpa@gmail.com>
2024-12-02 16:55:34 +0300

    feat: Update loginApi to use Credentials type and improve error handling in registerApi

--- frontend/src/features/account/apis/account.api.ts
+++ frontend/src/features/account/apis/account.api.ts
@@ -1,15 +1,17 @@
 import { User } from "../../../shared/lib/models";
 import { httpService } from "../../../shared/lib/services";
+import { Credentials } from "../../../shared/lib/types/crednetialds.type";
+import { APIError } from "../../../shared/lib/types/error.type";
 import { accountEndpoint } from "./api.endpoint";
 
 const handleResponse = (response: any) => response?.data;
 
-const handleError = (error: any) => {
+const handleError = (error: APIError) => {
   console.error("API call failed. Error: ", error);
   throw error;
 };
 
-export const loginApi = (email: string, password: string) => {
+export const loginApi = ({ email, password }: Credentials) => {
   return httpService
     .post(accountEndpoint.login, { email, password })
     .then(handleResponse)
@@ -17,10 +19,8 @@ export const loginApi = (email: string, password: string) => {
 };
 
 export const registerApi = async (user: User) => {
-  try {
-    const response = await httpService.post(accountEndpoint.register, user);
-    return handleResponse(response);
-  } catch (error) {
-    handleError(error);
-  }
+  return httpService
+    .post(accountEndpoint.register, user)
+    .then(handleResponse)
+    .catch(handleError);
 };
