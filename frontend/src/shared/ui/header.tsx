b00ad7a6e41728696f36a03c196886771880afbe
rabra <rabira.hierpa@gmail.com>
2024-12-02 16:55:55 +0300

    feat: Replace logo image with text for the header component

--- frontend/src/shared/ui/header.tsx
+++ frontend/src/shared/ui/header.tsx
@@ -7,11 +7,7 @@ const Header = () => {
       <div className=" container  mx-auto flex items-center h-14 space-x-6 	">
         <div className="text-primary-500 flex h-full items-center ">
           <Link to="/" className="text-lg font-semibold">
-            <img
-              src="assets/images/app-logo.png"
-              alt="fullstack-auth"
-              className="w-32 mx-auto"
-            />
+            <div className="text-lg text-red-500">FullStack Auth</div>
           </Link>
         </div>
         <div className=" flex flex-grow h-full items-center justify-end space-x-6">
