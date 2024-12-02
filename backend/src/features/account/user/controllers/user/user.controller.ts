45ebb22688872cc803819db37160cc0b8dfb3751
rabra <rabira.hierpa@gmail.com>
2024-12-02 16:29:20 +0300

    feat: Implement getCurrentUser endpoint to retrieve user details by email

--- backend/src/features/account/user/controllers/user/user.controller.ts
+++ backend/src/features/account/user/controllers/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Inject } from '@nestjs/common';
+import { User } from '../../decorators/user.decorator';
+import { UserService } from '../../services/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    @Inject(UserService)
+    private readonly userService,
+  ) {}
+
+  @Get('getCurrentUser')
+  async GetCurrentUser(@User() user) {
+    try {
+      return await this.userService.findByEmail(user.email);
+    } catch (ex) {
+      return new BadRequestException(ex);
+    }
+  }
+}
